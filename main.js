79c970fb6bf48e652d081366ddc7f493d47b2328
Xawwmal <137454797+Gustim4@users.noreply.github.com>
2026-03-25 15:29:42 +0700

    Refactor main.js for improved readability and structure

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function explodeText(x,y,text,isName){
   octx.textAlign="center";
   octx.fillStyle="white";
   octx.font = `bold ${
-  isMobile ? (isName?70:32) : (isName?160:82)
+  isMobile ? (isName?80:26) : (isName?160:82)
 }px Arial`;
   octx.fillText(text,off.width/2,off.height/2);
 
