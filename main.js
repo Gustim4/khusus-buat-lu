8dc7cb89dab60cb0c90681a2d5b822213315ab94
Xawwmal <137454797+Gustim4@users.noreply.github.com>
2026-03-25 15:10:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function explodeText(x,y,text,isName){
   octx.textAlign="center";
   octx.fillStyle="white";
   octx.font = `bold ${
-  isMobile ? (isName?90:50) : (isName?160:82)
+  isMobile ? (isName?70:40) : (isName?160:82)
 }px Arial`;
   octx.fillText(text,off.width/2,off.height/2);
 
